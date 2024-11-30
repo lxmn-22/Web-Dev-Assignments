1f0a04736162ac4aa0d1f3401d907fdca105caeb
Laxman Singh Koranga <korangalaxman03@gmail.com>
2024-12-01 02:41:59 +0530

    [Assignment07:random-password-generator]: Added project.

---
+++ Assignment07/random-password-generator/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div className='bg-green-500'>
+        <button className='w-full'>Generate Password</button>
+    </div>
+  )
+}
+
+export default Button
