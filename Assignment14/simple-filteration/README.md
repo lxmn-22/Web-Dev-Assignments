aa30f4346a56807231f62d74c81738ace4a6d512
Laxman Singh Koranga <korangalaxman03@gmail.com>
2025-02-28 23:39:46 +0530

    [ASSIGNMENT14]: Updated.

--- Assignment14/simple-filteration/README.md
+++ Assignment14/simple-filteration/README.md
@@ -1,1 +1,4 @@
 # REACT + Typescript (PROJECT-03)
+
+-   npm install
+-   npm install react-icons
--- Assignment14/simple-filteration/src/components/Sidebar.tsx
+++ Assignment14/simple-filteration/src/components/Sidebar.tsx
@@ -3,10 +3,10 @@ import React from "react";
 export const Sidebar = () => {
 	return (
 		<div className="w-16 fixed h-screen flex flex-col items-center border border-[#242424] p-4 space-y-6">
-			<div>Logo</div>
-			<div>📁</div>
-			<div>👤</div>
-			<div>⚙️</div>
+			<div className="text-white font-medium">Logo</div>
+			<div className="text-gray-400">📁</div>
+			<div className="text-gray-400">👤</div>
+			<div className="text-gray-400">⚙️</div>
 		</div>
 	);
 };
