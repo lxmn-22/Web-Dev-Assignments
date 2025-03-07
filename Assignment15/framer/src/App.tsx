82979c9e6c487688368aac8cee731855062b0b88
Laxman Singh Koranga <korangalaxman03@gmail.com>
2025-03-07 23:52:32 +0530

    [Assignment15:framer]: Added AnimatedCard & ImageGallery Component.

--- Assignment15/framer/src/App.tsx
+++ Assignment15/framer/src/App.tsx
@@ -1,9 +1,13 @@
+import AnimatedCard from "./components/AnimatedCard";
 import FlippingCard from "./components/FlippingCard";
+import ImageGallery from "./components/ImageGallery";
 
 function App() {
 	return (
-		<div className="min-h-screen flex items-center justify-center bg-gray-100">
+		<div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 gap-6">
 			<FlippingCard />
+			<AnimatedCard />
+			<ImageGallery />
 		</div>
 	);
 }
